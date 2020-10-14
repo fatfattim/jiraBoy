1655098bc451aa5696f259a7947a7a0ef244e350
TimChen <timchen@kkstream.com.tw>
2020-10-14 15:52:12 +0800

    To list the bad tickets

--- examineGoodTicket.py
+++ examineGoodTicket.py
@@ -4,8 +4,11 @@ from requests.auth import HTTPBasicAuth
 import json
 import slackWebhook
 
-# Purpose
-# 1. 
+# Purpose: You can find a ticket not have any "description" or issue links, it means that ticket's content is not a good story and clearly mission
+# To modify jql that adapt to your project setting
+
+userDictionary = { }
+
 def queryCommentsCount(startAt, maxResult):
     url = jiraConfig.httpResource["url"]
 
@@ -15,9 +18,11 @@ def queryCommentsCount(startAt, maxResult):
     }
 
     payload = json.dumps({
-        "jql": "project = OTP AND issuekey >= OTP-13 order by created DESC",
+        "jql": "project = OTP AND issuekey >= OTP-1999 AND status not in (Done, RESOLVED, \"Won't Fix\", Close) order by created DESC",
         "fields": [
-            "description"
+            "description",
+            "issuelinks",
+            "reporter"
         ],
         "startAt": startAt,
         "maxResults": maxResult
@@ -33,14 +38,22 @@ def queryCommentsCount(startAt, maxResult):
 
     #print(json.dumps(json.loads(response.text), sort_keys=True, indent=8, separators=(",", ": ")))
     for issue in issueArray:
-        description = issue['fields']['description']
+        fields = issue['fields']
+        key = issue['key']
+        reporterDisplayName = fields['reporter']['displayName']
+        description = fields['description']
         if description == None:
-            print("Empty description: " + issue['key'])
+            issuelinks = fields['issuelinks']
+            if issuelinks == None or len(issuelinks) == 0:
+                if userDictionary.get(reporterDisplayName) == None:
+                    userDictionary[reporterDisplayName] = [key]
+                else:
+                    userDictionary[reporterDisplayName].append(key)
             continue
         contentArray = description['content']
         
-        if len(contentArray) < 3:
-            print("Less information: " + issue['key'])
+        if len(contentArray) < 1:
+            print("Less information: " + key)
     return jsonText['total']
 
 maxResult = 100
@@ -52,4 +65,11 @@ while startAt + maxResult < totalCount:
     startAt += maxResult 
     queryCommentsCount(startAt, maxResult)
 
-# slackWebhook.sendToSlack(slackMessage)
+slackMessage = "Please enhance the ticket's description or give links which help reviewer get more information \n " 
+
+for key, value in userDictionary.items():
+  appendStr = key + ": " + ', '.join(value) + "\n"
+  slackMessage += appendStr
+
+print(slackMessage)
+#slackWebhook.sendToSlack(slackMessage)
